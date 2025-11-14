e71e17adf2dc2786a07ee4bdc38f4982456e7ab6
Peixotim <pedropeixotovz@gmail.com>
2025-11-14 14:24:12 -0300

    fix:Format

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-images: {
+  images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**', // Permite qualquer imagem desse host
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**", // Permite qualquer imagem desse host
       },
       {
-        protocol: 'https',
-        hostname: 'placehold.co', // Adicionado para o seu fallback 'onError'
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co", // Adicionado para o seu fallback 'onError'
+        port: "",
+        pathname: "/**",
       },
     ],
   },
 };
-
 
 export default nextConfig;

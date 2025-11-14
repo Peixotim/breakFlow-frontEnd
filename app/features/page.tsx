b14e86d0d5ae7167611cb430045a05b7814141c5
Peixotim <pedropeixotovz@gmail.com>
2025-11-14 14:24:34 -0300

    fix:Format

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -6,21 +6,15 @@ import { FeaturesPageHero } from "@/components/features-page-hero";
 import { FeaturesGrid } from "@/components/features-grid";
 export default function FeaturesPage() {
   return (
-    <div className="min-h-screen w-full overflow-hidden 
-                    bg-gray-50 dark:bg-[#121214] 
-                    text-gray-900 dark:text-[#EDEDED] 
-                    selection:bg-[#89F336]/30">
-      
+    <div className="min-h-screen w-full overflow-hidden bg-gray-50 text-gray-900 selection:bg-[#89F336]/30 dark:bg-[#121214] dark:text-[#EDEDED]">
       <Header />
 
       <main>
         <FeaturesPageHero />
         <FeaturesGrid />
-        
       </main>
 
       <Footer />
-
     </div>
   );
 }

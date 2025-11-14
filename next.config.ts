dd7a78ce428875eb0014272440d79d73af052519
Peixotim <pedropeixotovz@gmail.com>
2025-11-14 14:19:45 -0300

    feat: Adiciona a 'permissao', para utilizar imagens do unsplash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**', // Permite qualquer imagem desse host
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // Adicionado para o seu fallback 'onError'
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
+
 
 export default nextConfig;

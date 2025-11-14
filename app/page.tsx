bf229b6a82f25453c2b65d9fd31cc2d6271f3017
Peixotim <pedropeixotovz@gmail.com>
2025-11-14 11:39:21 -0300

    feat: Adiciona os componentes a pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  <></>;
+"use client";
+
+import { Header } from "@/components/header";
+import { HeroSection } from "@/components/hero-section";
+import { FeaturesSection } from "@/components/features-section";
+import { TestimonialsSection } from "@/components/testimonials-section";
+import { PricingSection } from "@/components/pricing-section"; // 1. Importe
+import { Footer } from "@/components/Footer"; 
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen w-full overflow-hidden 
+                    bg-gray-50 dark:bg-[#121214] 
+                    text-gray-900 dark:text-[#EDEDED] 
+                    selection:bg-[#89F336]/30">
+      
+      <Header />
+
+      <main>
+        <HeroSection />
+        <FeaturesSection />
+        <TestimonialsSection />
+        <PricingSection /> 
+      </main>
+
+      <Footer />
+
+    </div>
+  );
 }
